b94af1e8886372b5164e5a117267099d4aef7ac7
Zanzouneto <saadezeina@hotmail.com>
2024-10-01 17:34:45 +0300

    .

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ $(window).scroll(function(e){
 
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
-    $('.y1').css({'transform': 'translate3d(0,' + scrolled * -0.6 + 'px, 0)'});
-    $('.y2').css({'transform': 'translate3d(0,' + scrolled * -1.0 + 'px, 0)'}); 
-    $('.y3').css({'transform': 'translate3d(0,' + scrolled * -1.2 + 'px, 0)'});  
-    $('.y4').css({'transform': 'translate3d(0,' + scrolled * -1.6 + 'px, 0)'});  
-    $('.y5').css({'transform': 'translate3d(0,' + scrolled * -2.0 + 'px, 0)'});  
+    $('.y1').css({'transform': 'translate3d(0,' + scrolled * -1.9 + 'px, 0)'});
+    $('.y2').css({'transform': 'translate3d(0,' + scrolled * -2 + 'px, 0)'}); 
+    $('.y3').css({'transform': 'translate3d(0,' + scrolled * -2.1 + 'px, 0)'});  
+    $('.y4').css({'transform': 'translate3d(0,' + scrolled * -2.2 + 'px, 0)'});  
+    $('.y5').css({'transform': 'translate3d(0,' + scrolled * -2.3 + 'px, 0)'});  
     $('.y6').css({'transform': 'translate3d(0,' + scrolled * -2.4 + 'px, 0)'});  
 
     $('.title').css({'transform': 'translate3d(0,' + scrolled * -0.5 + 'px, 0)'});  
